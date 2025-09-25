1658f78477da887c59fd08eb0cb0f6c55d087de8
mpalmer79 <mpalm2025@gmail.com>
2025-09-25 17:36:30 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,28 @@
-console.log("Quirk Sidecar service worker alive");
-chrome.runtime.onInstalled.addListener(() => console.log("Installed/updated"));
+// background.js (service worker)
+chrome.runtime.onInstalled.addListener(() => {
+  // Right-click menu for selected text
+  chrome.contextMenus.create({
+    id: 'quirk-open',
+    title: 'Send to Quirk panel',
+    contexts: ['selection'],
+  });
+});
 
+// Open/Toggle panel when the toolbar icon is clicked
+chrome.action.onClicked.addListener((tab) => {
+  if (tab?.id) chrome.tabs.sendMessage(tab.id, { type: 'toggle-panel' });
+});
+
+// Context-menu -> send selection into panel
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'quirk-open' && tab?.id) {
+    chrome.tabs.sendMessage(tab.id, { type: 'open-with-selection', text: info.selectionText || '' });
+  }
+});
+
+// Keyboard shortcut (Alt+Q by default)
+chrome.commands.onCommand.addListener((command, tab) => {
+  if (command === 'toggle-quirk-panel' && tab?.id) {
+    chrome.tabs.sendMessage(tab.id, { type: 'toggle-panel' });
+  }
+});
